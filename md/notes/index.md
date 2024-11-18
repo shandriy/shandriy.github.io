655a296d878ff0aa3beff7c0e4676601a322763d
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-17 17:15:57 -0800

    Add first blog post

---
+++ md/notes/index.md
@@ -0,0 +1,8 @@
+generic.htm
+
+## Notes
+
+### 11/2024
+
+- 17/11/2024 - [The Most Cross-Compatible Font Stacks](/notes/2024/11/17.htm)
+
---
+++ music/HUMBLE.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="HUMBLE."
+song_of_the_day_album="DAMN."
+song_of_the_day_artist="Kendrick Lamar"
+song_of_the_day_spotify="https://open.spotify.com/track/7KXjTSCq5nL1LoYtL7XAwS?si=0ba2198d47f34056"
+song_of_the_day_youtube="https://youtu.be/ov4WobPqoSA?si=8oqT8-Ta0L0NPRl_"
