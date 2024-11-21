cbd1a126f6a8c0e532bf91ecfc84edfb5dde71ef
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-20 17:52:59 -0800

    Update song of the day

---
+++ music/glitter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="glitter ✩‧₊˚ •"
+song_of_the_day_album="glitter ✩‧₊˚ •"
+song_of_the_day_artist="lexycat"
+song_of_the_day_spotify="https://open.spotify.com/track/1Myqi1Jjaz7n3M67BXQI4C?si=a038084dfbf3414c"
+song_of_the_day_youtube="https://youtu.be/6FUrVU7vr5Y?si=H1wSWwjfU5iAUWDq"
