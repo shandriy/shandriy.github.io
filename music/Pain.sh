d3e37ad55ca2f83f8c47c128a9a4d0625c5b4534
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-21 17:21:23 -0800

    Update song of the day

---
+++ music/Pain.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Pain"
+song_of_the_day_album="to hell with it"
+song_of_the_day_artist="PinkPantheress"
+song_of_the_day_spotify="https://open.spotify.com/track/4S4QJfBGGrC8jRIjJHf1Ka?si=2c675142bc554e9a"
+song_of_the_day_youtube="https://youtu.be/lw_XFnk5kwU?si=cRpCIL3Gc_Gt2Otv"
