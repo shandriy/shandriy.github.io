c7965714dcd6e9ecd17f28fb1160b980098a140d
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-23 15:04:42 -0800

    Update song of the day

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ DATE=$(date +"%F %T.%N %::z")
 SECONDS=$(date +"%s")
 FOOTER_TEXT=$(cat ./src/footer-text.htm)
 
-SONG="Liquid_Smooth"
+SONG="wanting_getting_wanting"
 
 # Get song script
 
---
+++ music/wanting_getting_wanting.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="wanting, getting, wanting"
+song_of_the_day_album="SHUT UP, GET HAPPY"
+song_of_the_day_artist="DEMONDICE"
+song_of_the_day_spotify="https://open.spotify.com/track/3o9dGOt9zwCaIH4XYmuLpb?si=d1c93c1a6ffa49ca"
+song_of_the_day_youtube="https://youtu.be/RXu62hENi1Y?si=MysGbtRW857o3xr_"
