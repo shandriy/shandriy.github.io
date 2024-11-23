f426bf03adb4563b6b285e43a34f46260a903072
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-22 20:29:25 -0800

    Update song of the day

---
+++ music/Liquid_Smooth.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Liquid Smooth"
+song_of_the_day_album="Lush"
+song_of_the_day_artist="Mitski"
+song_of_the_day_spotify="https://open.spotify.com/track/3XCveEutwTaDiekRkkfdp9?si=932f418fbe4d4edc"
+song_of_the_day_youtube="https://youtu.be/Bfurc6KcMwk?si=KMAYnw_76gro951U"
