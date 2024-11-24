df5b7cb82ec7d407c74632988447afdeacc2bdc2
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-24 11:58:59 -0800

    Update song of the day

---
+++ music/Eyes_That_Gaze_into_the_Nexus_The_Ones_Who_Gather_Stars_in_the_Night.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Eyes That Gaze into the Nexus -- The Ones Who Gather Stars in the Night"
+song_of_the_day_album="BRAVELY DEFAULT II Original Soundtrack"
+song_of_the_day_artist="Revo"
+song_of_the_day_spotify="https://open.spotify.com/track/6nKoCknKjXcA6saQZ6AyZA?si=7fb2698620b5465b"
+song_of_the_day_youtube="https://youtu.be/5OkSmLK1jUg?si=Lxy--sDrHffQwRo_"
