8039ba18b075b22ea02ec85d2416730df8882de8
shandriy <151408521+shandriy@users.noreply.github.com>
2024-11-27 18:22:31 -0800

    Update song of the day

---
+++ music/tv_off_feat_lefty_gunplay.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="tv off (feat. lefty gunplay)"
+song_of_the_day_album="GNX"
+song_of_the_day_artist="Kendrick Lamar"
+song_of_the_day_spotify="https://open.spotify.com/track/0aB0v4027ukVziUGwVGYpG?si=8be989afc7f94b49"
+song_of_the_day_youtube="https://youtu.be/U8F5G5wR1mk?si=hdp7ECgUdTq6gcOv"
