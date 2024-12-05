34c81a4d9546c4f2757ab4e3b8557d80b397df58
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-04 17:53:50 -0800

    Update song of the day

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ DATE=$(date +"%F %T.%N %::z")
 SECONDS=$(date +"%s")
 FOOTER_TEXT=$(cat ./src/footer-text.htm)
 
-SONG="euphoria"
+SONG="Im_so_Concerned_About_the_Ending_That_I_Dont_Even_Know_the_Plot"
 
 # Get song script
 
---
+++ music/Im_so_Concerned_About_the_Ending_That_I_Dont_Even_Know_the_Plot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="I'm so Concerned About the Ending That I Don't Even Know the Plot"
+song_of_the_day_album="I Stayed Up Until Sunrise But Got To Fall Asleep To The Sounds Of Birds Singing"
+song_of_the_day_artist="Flatsound"
+song_of_the_day_spotify="https://open.spotify.com/track/1TkrPsgEioLP4W7jLBw3Gm?si=b96c7300ae6c46a7"
+song_of_the_day_youtube="https://youtu.be/jSZ_nQG3e1s?si=UN122-c1j8pBrgws"
