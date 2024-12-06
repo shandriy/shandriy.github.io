070c70114177d05c6b6d2848a0c7a12907208d1c
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-05 20:08:23 -0800

    Update song of the day

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ DATE=$(date +"%F %T.%N %::z")
 SECONDS=$(date +"%s")
 FOOTER_TEXT=$(cat ./src/footer-text.htm)
 
-SONG="Im_so_Concerned_About_the_Ending_That_I_Dont_Even_Know_the_Plot"
+SONG="Overkill"
 
 # Get song script
 
---
+++ music/Overkill.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Overkill"
+song_of_the_day_album="Overkill"
+song_of_the_day_artist="Mori Calliope"
+song_of_the_day_spotify="https://open.spotify.com/track/1UlQyJezbjrwRlC2fo3D3i?si=3a990e4721594361"
+song_of_the_day_youtube="https://youtu.be/J7tp_0lFI0I?si=UhteaayxolZyoj07"
