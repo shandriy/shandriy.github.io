0ad18ba74cce6d7a79770f804597b99275ef8ef4
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-06 19:34:01 -0800

    Update song of the day

---
+++ music/Art_Deco.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Art Deco"
+song_of_the_day_album="Honeymoon"
+song_of_the_day_artist="Lana Del Ray"
+song_of_the_day_spotify="https://open.spotify.com/track/5jqNQZBwbZWQXPWfo0ygZF?si=127547269b944b82"
+song_of_the_day_youtube="https://youtu.be/QbLGjeR9bvI?si=L-ufxc5XeSxw_o8V"
