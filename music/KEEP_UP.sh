6aee5cdee97107c996789b8cc4d06d27e34e0918
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-08 11:40:49 -0800

    Update song of the day

---
+++ music/KEEP_UP.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="KEEP UP"
+song_of_the_day_album="KEEP UP // FROSTBITE"
+song_of_the_day_artist="Odetari"
+song_of_the_day_spotify="https://open.spotify.com/track/2yR2sziCF4WEs3klW1F38d?si=97898274a40d4985"
+song_of_the_day_youtube="https://youtu.be/sYpSpHq9ziM?si=_3LThQngqmFRWxu0"
