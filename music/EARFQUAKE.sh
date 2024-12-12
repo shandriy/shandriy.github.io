9f886d70369c57c3316258e64d57e90125e06402
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-11 21:33:15 -0800

    Update song of the day

---
+++ music/EARFQUAKE.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="EARFQUAKE"
+song_of_the_day_album="IGOR"
+song_of_the_day_artist="Tyler, The Creator"
+song_of_the_day_spotify="https://open.spotify.com/track/5hVghJ4KaYES3BFUATCYn0?si=8d6d075b88914dd1"
+song_of_the_day_youtube="https://youtu.be/HmAsUQEFYGI?si=DnRK6zEJev9Pg6aq"
