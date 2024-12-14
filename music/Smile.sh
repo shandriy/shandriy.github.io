16f42d30d9a4b9aefeef078bec82afbb584e1b1c
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-13 19:00:56 -0800

    Update song of the day

---
+++ music/Smile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Smile"
+song_of_the_day_album="Alright, Still"
+song_of_the_day_artist="Lily Allen"
+song_of_the_day_spotify="https://open.spotify.com/track/4aR9bPMAOFySBuQSbVWF3d?si=4c91390913c64ad6"
+song_of_the_day_youtube="https://youtu.be/0WxDrVUrSvI?si=mQat8WLCmgWTWvA_"
