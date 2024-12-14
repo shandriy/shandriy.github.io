aac343041437c30044ad54d9e8b96c7c6168c40d
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-14 12:00:02 -0800

    Update song of the day

---
+++ music/Neva.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Нева"
+song_of_the_day_album="Пятница"
+song_of_the_day_artist="5'nizza"
+song_of_the_day_spotify="https://open.spotify.com/track/3Nee8plW0d5Gsu6Txx4hUi?si=34acc8b4f7804b79"
+song_of_the_day_youtube="https://youtu.be/pltTzVz8mEA?si=5RxOcERtEt6uM_Cd"
