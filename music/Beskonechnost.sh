fee0275edfd815147caa44111e64f4b986bbef0c
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-19 18:56:00 -0800

    Update song of the day

--- music/Beskonechnost.sh
+++ music/Beskonechnost.sh
@@ -2,6 +2,6 @@
 
 song_of_the_day_name="&infin;"
 song_of_the_day_album="четырнадцать недель тишины"
-song_of_the_day_artist="Zemfira"
+song_of_the_day_artist="Земфира"
 song_of_the_day_spotify="https://open.spotify.com/track/1zWoGo3R1tAsgsnyPeZkw6?si=3d994494b77e486f"
 song_of_the_day_youtube="https://youtu.be/A29cDw57qWQ?si=58D6zCCopBD7QiF0"
