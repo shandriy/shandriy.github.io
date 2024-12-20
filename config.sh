e85ad5de83e65a01d80a4a24f067acdbedd58c31
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-20 09:48:53 -0800

    Update song of the day

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ DATE=$(date +"%F %T.%N %::z")
 SECONDS=$(date +"%s")
 FOOTER_TEXT=$(cat ./src/footer-text.htm)
 
-SONG="Freak"
+SONG="HOT_TO_GO"
 
 # Get song script
 
---
+++ music/HOT_TO_GO.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+song_of_the_day_name="HOT TO GO!"
+song_of_the_day_album="The Rise and Fall of a Midwest Princess"
+song_of_the_day_artist="Chappell Roan"
+song_of_the_day_spotify="https://open.spotify.com/track/4xdBrk0nFZaP54vvZj0yx7?si=c2b917fc3fbe4066"
+song_of_the_day_youtube="https://youtu.be/xaPNR-_Cfn0?si=ERpd5elV5KAN_IS0"
+song_of_the_day_quote="H-O-T-T-O-G-O, You can take me hot to go!"
