1cf71960ee07e232b6b6b58f25525f66972fa08a
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-22 19:47:06 -0800

    Update song of the day

---
+++ music/Bad_At_Love.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Bad At Love"
+song_of_the_day_album="hopeless fountain kingdom"
+song_of_the_day_artist="Halsey"
+song_of_the_day_spotify="https://open.spotify.com/track/3OzCF9i6Ey7EkkAYJztmKp?si=042934a2ecb040b3"
+song_of_the_day_youtube="https://youtu.be/xdYFuCp3m9k?si=LXNUaD65_oGmQ6T8"
