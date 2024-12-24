5623cd8ac49d84b23ab19f62b9b8d4f533bb9257
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-23 18:23:50 -0800

    Update song of the day

---
+++ music/Magnetic.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+song_of_the_day_name="Magnetic"
+song_of_the_day_album="SUPER REAL ME"
+song_of_the_day_artist="ILLIT"
+song_of_the_day_spotify="https://open.spotify.com/track/1aKvZDoLGkNMxoRYgkckZG?si=54829180d31f48ab"
+song_of_the_day_youtube="https://youtu.be/Vk5-c_v4gMU?si=QtaOLL7tTRF3tK3T"
+song_of_the_day_quote="I'm just trying to play it cool - but I just couldn't hide that - I want you"
