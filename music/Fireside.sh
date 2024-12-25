87526a5d52d7f74bccb0416ad892565a04ac89da
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-25 12:20:37 -0800

    Update song of the day

---
+++ music/Fireside.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+song_of_the_day_name="Fireside"
+song_of_the_day_album="AM"
+song_of_the_day_artist="Arctic Monkeys"
+song_of_the_day_spotify="https://open.spotify.com/track/4atMrAadB7dS8xn9vfk9PQ?si=da2eacc5425d4dfe"
+song_of_the_day_youtube="https://youtu.be/8-LewaeKejM?si=u_-TelqFxxpSicPu"
+song_of_the_day_quote="I'm not sure if I should, show you what I found"
