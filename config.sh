6d9e177904bb43c3d539ee804660cb10807826b9
shandriy <151408521+shandriy@users.noreply.github.com>
2024-12-27 20:41:45 -0800

    Update song of the day

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ DATE=$(date +"%F %T.%N %::z")
 SECONDS=$(date +"%s")
 FOOTER_TEXT=$(cat ./src/footer-text.htm)
 
-SONG="Fireside"
+SONG="Bonfire"
 
 # Get song script
 
---
+++ music/Bonfire.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="Bonfire"
+song_of_the_day_album="Camp"
+song_of_the_day_artist="Childish Gambino"
+song_of_the_day_spotify="https://open.spotify.com/track/0wps7rop5iHwBo0rQZ2g9A?si=a4ab78d10c024f9c"
+song_of_the_day_youtube="https://youtu.be/qZFVb8PGxv0?si=5K7u3aUso6BPM7CG"
