bdf0b8849be5cf1e5c644bf4cba98399dbb4024c
shandriy <151408521+shandriy@users.noreply.github.com>
2025-01-07 18:35:07 -0800

    Update song of the day

---
+++ music/TiK_ToK.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+song_of_the_day_name="TiK ToK"
+song_of_the_day_album="Animal"
+song_of_the_day_artist="Ke\$sha"
+song_of_the_day_spotify="https://open.spotify.com/track/0HPD5WQqrq7wPWR7P7Dw1i?si=20e97479a58b4dcc"
+song_of_the_day_youtube="https://youtu.be/iP6XpLQM2Cs?si=1MWUUSIxThMjsvoI"
